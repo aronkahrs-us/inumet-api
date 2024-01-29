fff3ce01b84078cc49c417c0ae8ddaa85b51ba23
Aron Kahrs <aronkahrs.us@gmail.com>
2024-01-29 16:07:33 -0300

    added test

--- src/inumet_api/api.py
+++ src/inumet_api/api.py
@@ -3,42 +3,39 @@ import requests
 BASE_URL = "https://www.inumet.gub.uy"
 
 class INUMET:
-    def __init__(self,zone:str="", station:str="") -> None:
+    def __init__(self,zone:int=None, station:int=None) -> None:
         self.zone = zone
         self.station = station
         self._endpoints()
         pass
-    
+
     def estaciones(self):
         return self._request(self.endpoints['estaciones'])
-    
+
     def zonas(self):
         return self._request(self.endpoints['zonas'])
-    
+
     def departamentos(self):
         return self._request(self.endpoints['departamentos'])
-    
-    def estado_actual(self, station:str=""):
+
+    def estado_actual(self):
         data = self._request(self.endpoints['estadoactual'])
         date = data['Fecha']
         time = data['Hora']
-        if station != "":
-            return [x for x in data['estaciones'] if x['estacion'] == station ][0]
+        if self.station:
+            return [x for x in data['estaciones'] if x['estacion'] == self.station ][0]
         else:
             return data['estaciones']
-    
-    def pronostico(self, id:int = None):
+
+    def pronostico(self):
         data = self._request(self.endpoints['pronosticoV2'].replace('.mch','.json'))
-        if id and id in [x['idInt'] for x in self.zonas()['zonas']]:
-            data=[x for x in data['items'] if x['zonaId'] == id]
+        if self.zone and self.zone in [x['idInt'] for x in self.zonas()['zonas']]:
+            data=[x for x in data['items'] if x['zonaId'] == self.zone]
             return data
-        elif id:
+        elif self.zone:
             raise ValueError
         else:
             return data['items']
-    
-    def _pronostico_old(self):
-        return self._request(self.endpoints['pronostico'])
 
     def advertencias(self):
         data=self._request(self.endpoints['nivelRiesgoV2'])
@@ -48,12 +45,23 @@ class INUMET:
         mapa = data['mapaMerge']
         return pronosticador,advertencias,actualizacion,mapa
     
+    def _test(self):
+        if self.station not in [x['id'] for x in self.estaciones()['estaciones']]:
+            raise ValueError
+        if self.zone not in [x['idInt'] for x in self.zonas()['zonas']]:
+            raise ValueError
+        return True
+
+
+    def _pronostico_old(self):
+        return self._request(self.endpoints['pronostico'])
+
     def _advertencias_old(self):
         return self._request(self.endpoints['nivelRiesgo'])
-    
+
     def _endpoints(self):
         self.endpoints = {x['nombre']: x['url'] for x in self._request("/android/info_recursosV5.json")['info_recursos']}
         return self.endpoints
-    
+
     def _request(self,path:str):
         return requests.get(url=f"{BASE_URL}/{path}").json()
